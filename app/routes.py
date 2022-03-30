59d87a39e3cbcc6bdf67af8ed3bbb1057186bf54
Mirkogilioli <mirko.gilioli@gmail.com>
2022-03-30 11:11:45 +0200

    A new dev page has been introduced

--- app/routes.py
+++ app/routes.py
@@ -3,3 +3,7 @@ from app import flask_app
 @flask_app.route('/')
 def home():
     return 'Wecolme to Google Cloud DevOps!'
+
+@flask_app.route('/dev')
+def devPage():
+    return 'Welcome this is the Development Page'
