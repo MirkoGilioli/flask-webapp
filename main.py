f12893b03af85fd12c467c53e23d965a8cdddad9
Mirkogilioli <mirko.gilioli@gmail.com>
2022-04-04 10:55:50 +0200

    Removed google cloud profiler

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
 from app import flask_app
-import googlecloudprofiler
-
-try:
-    googlecloudprofiler.start(verbose=3)
-except (ValueError, NotImplementedError) as exc:
-    print(exc)
 
 if __name__ == '__main__':
     flask_app.run(debug=True, host='0.0.0.0', port=8080)
