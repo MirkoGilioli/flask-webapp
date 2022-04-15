8315e64f2a1972e69de31244cc5cbbee138b521f
Mirkogilioli <mirko.gilioli@gmail.com>
2022-04-15 16:12:54 +0200

    cloudprofiler has been introduced for monitoring

--- Dockerfile
+++ Dockerfile
@@ -6,7 +6,10 @@ COPY main.py .
 COPY requirements.txt .
 ADD ./app app
 
+RUN apt-get update && apt-get install -y build-essential python3-pip
+RUN pip install google-cloud-profiler
 RUN pip install -r requirements.txt
+
 EXPOSE 8080/tcp
 
 CMD ["python", "main.py"]
--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 from app import flask_app
+import googlecloudprofiler
 
 if __name__ == '__main__':
+
+    # Profiler initialization. It starts a daemon thread which continuously
+    # collects and uploads profiles. Best done as early as possible.
+    try:
+        googlecloudprofiler.start(
+            service='webapp',
+            # verbose is the logging level. 0-error, 1-warning, 2-info, 3-debug. It defaults to 0 (error) if not set.
+            verbose=3,
+            # project_id must be set if not running on GCP.
+            # project_id='my-project-id',
+        )
+    except (ValueError, NotImplementedError) as exc:
+        print(exc)  # Handle errors here
     flask_app.run(debug=True, host='0.0.0.0', port=8080)
