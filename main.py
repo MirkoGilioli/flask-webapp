db770b7adadfea3945762a7bb251a1aa8c814a58
Mirkogilioli <mirko.gilioli@gmail.com>
2022-04-27 16:40:58 +0200

    cloud debugger running in normal mode

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
         print(exc)  # Handle errors here
 
     try:
-        googleclouddebugger.enable(breakpoint_enable_canary=True)
+        googleclouddebugger.enable()
     except ImportError:
         pass
     flask_app.run(debug=True, host='0.0.0.0', port=8080)
